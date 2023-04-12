59d19b5adae4d215fb005030c403da346a07779a
ReborN <noreply@frostoven.com>
2023-04-12 12:42:06 +0100

    #9 added Semantic UI tabs; Auto updates name with controller name

---
+++ app/react/components/GamepadTabs.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react'
+import {Menu} from 'semantic-ui-react'
+
+export default class GamepadTabs extends Component {
+  state = {
+    activeItem: null,
+    gamepads: new Array(4).fill(null),
+  }
+
+  componentDidMount() {
+    window.addEventListener('gamepadconnected', this.handleGamepadConnected)
+    window.addEventListener('gamepaddisconnected', this.handleGamepadDisconnected)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('gamepadconnected', this.handleGamepadConnected)
+    window.removeEventListener('gamepaddisconnected', this.handleGamepadDisconnected)
+  }
+
+  handleGamepadConnected = (event) => {
+    const gamepad = event.gamepad
+    const gamepads = [...this.state.gamepads]
+    gamepads[gamepad.index] = gamepad
+    this.setState({gamepads})
+    if (gamepad.index === 0) {
+      this.setState({activeItem: `Controller ${gamepad.index + 1}: ${gamepad.id}`})
+    }
+  }
+
+  handleGamepadDisconnected = (event) => {
+    const gamepad = event.gamepad
+    const gamepads = [...this.state.gamepads]
+    gamepads[gamepad.index] = null
+    this.setState({gamepads})
+    if (this.state.activeItem === `Controller ${gamepad.index + 1}: ${gamepad.id}`) {
+      this.setState({activeItem: null})
+    }
+  }
+
+  handleItemClick = (e, {name}) => this.setState({activeItem: name})
+
+  render() {
+    const {activeItem, gamepads} = this.state
+
+    // @ts-ignore
+    const menuItems: React.ReactNode[] = gamepads.map((gamepad, index) => {
+      const name = gamepad ? `Controller ${index + 1}: ${gamepad.id}` : `Controller ${index + 1}`
+      return (
+        <Menu.Item
+          key={index}
+          name={name}
+          active={activeItem === name}
+          onClick={this.handleItemClick}
+        />
+      )
+    })
+
+    return (
+      <Menu>
+        {menuItems}
+      </Menu>
+    )
+  }
+}
