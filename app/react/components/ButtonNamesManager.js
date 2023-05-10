7657c7e66dbc7546e9b9c7ef4715d775a51c3817
ReborN <reborn@frostoven.com>
2023-05-10 13:22:12 +0100

    #21 App now saves & loads btn names on restart.

--- app/react/components/ButtonNamesManager.js
+++ app/react/components/ButtonNamesManager.js
@@ -1,12 +1,10 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import path from 'path';
 import { getUserDataDir } from '../../userProfile';
 
 const userDataDir = getUserDataDir();
 const buttonNamesFilePath = path.join(userDataDir, 'button_names.json');
 const fs = require('fs');
-const buttonNamesFilename = 'buttonNames.json';
-const buttonNamesFilepath = path.join(getUserDataDir(), buttonNamesFilename);
 
 const readButtonNamesFromFile = () => {
   try {
@@ -18,32 +16,30 @@ const readButtonNamesFromFile = () => {
   }
 };
 
-function saveButtonNamesToFile(buttonNames) {
+const saveButtonNamesToFile = (buttonNames) => {
   try {
-    const data = JSON.stringify(buttonNames);
-    fs.writeFileSync(buttonNamesFilepath, data, 'utf8');
+    const data = JSON.stringify(buttonNames, null, 2);
+    fs.writeFileSync(buttonNamesFilePath, data, 'utf-8');
   } catch (err) {
     console.error('Error writing button names file:', err);
   }
-}
+};
 
 export const ButtonNamesManager = ({ gamepad }) => {
   const [buttonNames, setButtonNames] = useState(gamepad ? Array(gamepad.buttons.length).fill("") : []);
 
   useEffect(() => {
     if (gamepad) {
-      const storedButtonNames = readButtonNamesFromFile();
+      const storedButtonNames = readButtonNamesFromFile(gamepad);
       setButtonNames(storedButtonNames);
     }
   }, [gamepad]);
 
-  const handleRenameButtonClick = (index, newName) => {
+  const handleRenameButtonClick = (index, newName, isAxis) => {
     setButtonNames((prevButtonNames) => {
-      if (!Array.isArray(prevButtonNames)) {
-        prevButtonNames = [];
-      }
-      const newButtonNames = [...prevButtonNames];
-      newButtonNames[index] = newName;
+      const key = isAxis ? `ax${index}` : `bt${index}`;
+      const newButtonNames = { ...prevButtonNames };
+      newButtonNames[key] = newName;
       saveButtonNamesToFile(newButtonNames);
       return newButtonNames;
     });
