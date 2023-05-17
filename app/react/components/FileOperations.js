1cb053777a0ec532c6cea3195b56c7c3c9053d27
ReborN <reborn@frostoven.com>
2023-05-17 10:37:49 +0100

    #22 Refactored file operations and directory management into FileOperations.js

---
+++ app/react/components/FileOperations.js
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import path from 'path';
+import userProfile from '../../userProfile'
+import { generateDefaultButtonNames } from './ButtonNamesManager'
+
+const profileDir = path.join(userProfile.getUserDataDir(), 'Frostoven/GamepadProfiler');
+const buttonNamesFilePath = path.join(profileDir, 'button_names.json');
+
+export const checkAndCreateDir = async () => {
+  try {
+    await fs.promises.access(profileDir);
+  } catch (e) {
+    await fs.promises.mkdir(profileDir, {recursive: true});
+  }
+};
+
+const readButtonNamesFromFile = async (gamepad) => {
+    const defaultButtonNames = generateDefaultButtonNames(gamepad);
+    try {
+      const data = await fs.promises.readFile(buttonNamesFilePath, 'utf-8');
+      const storedButtonNames = JSON.parse(data);
+      return {...defaultButtonNames, ...storedButtonNames};
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        console.log('File not found, returning default button names');
+        return defaultButtonNames;
+      } else {
+        console.error('Error reading button names file:', err);
+      }
+    }
+};
+
+const saveButtonNamesToFile = async (buttonNames) => {
+  try {
+    const data = JSON.stringify(buttonNames, null, 2);
+    await fs.promises.writeFile(buttonNamesFilePath, data, 'utf-8');
+  } catch (err) {
+    console.error('Error writing button names file:', err);
+  }
+};
+
+export {
+  saveButtonNamesToFile,
+  readButtonNamesFromFile
+}
